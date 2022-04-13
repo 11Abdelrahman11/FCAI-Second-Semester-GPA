6009817ee4513c09a366e3fef2f3ea6d72d4ecf7
11Abdelrahman11 <92861323+11Abdelrahman11@users.noreply.github.com>
2022-04-13 21:01:27 +0200

    Add an Input Protection and an advanced format
    Making a Function to Check if the Input is Integer or not so We Can make Our Mathematical Operations on it

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 double degree (string subject);
 int intLength( int N );
+bool is_Integer (string number);
+int Length ( double n );
 
 int main()
 {
@@ -120,7 +122,7 @@ int main()
             cout << "GPA" << setw(14) << "" << "| " << GPA << setw(9) << "" << "| " << grades[8] << endl;
         }
         else{
-            cout << "GPA" << setw(14) << "" << "| " << setw(3) << GPA << setw(7) << "" << "| " << grades[8] << endl;
+            cout << "GPA" << setw(14) << "" << "| " << GPA << setw(10 - Length(GPA)) << "" << "| " << grades[8] << endl;
         }
         cout << setfill('-');
         cout << setw(36) << "" << endl;
@@ -136,17 +138,56 @@ return 0;
 }
 
 double degree (string subject){
-    int degree;
+    string degreeVal;
     cout << "Enter " << subject << " Degree: ";
-    cin >> degree;
-    while(degree > 100 || degree < 0){
-        cout << "Please Insure to Enter A Valid Degree (0-100): ";
-        cin >> degree;
+    cin >> degreeVal;
+    int Intdegree;
+    if (is_Integer(degreeVal) == false){
+        while(is_Integer(degreeVal) == false){
+            cout << "Please Insure to Enter A Valid Degree (number between (0-100)): ";
+            cin >> degreeVal;
+            if (is_Integer(degreeVal)){
+                Intdegree = stoi(degreeVal);
+                while(Intdegree > 100 || Intdegree < 0){
+                    cout << "Please Insure to Enter A Valid Degree (number between (0-100)): ";
+                    cin >> Intdegree;
+                }
+            }
+        }
     }
-    return degree;
+    else{
+        Intdegree = stoi(degreeVal);
+        while(Intdegree > 100 || Intdegree < 0){
+            cout << "Please Insure to Enter A Valid Degree (number between (0-100)): ";
+            cin >> Intdegree;
+        }
+    }
+    return Intdegree;
+}
+bool is_Integer(string number){
+    bool is_integer = false;
+    for (int i = 0; i < number.length(); i++){
+        if (isdigit(number[i]) == false){
+            is_integer = false;
+            break;
+        }
+        else{
+            is_integer = true;
+        }
+    }
+    return is_integer;
 }
 int intLength ( int N ){
     if      ( N < 0  ) return 1 + intLength( -N );
     else if ( N < 10 ) return 1;
     else               return 1 + intLength( N / 10 );
+}
+int Length ( double n){  // just for double type
+    string str_Number;
+    str_Number = to_string(n);
+    int length = 0;
+    for (char i = 0; i < str_Number.length(); i++){
+        length++;
+    }
+    return length-2;
 }
